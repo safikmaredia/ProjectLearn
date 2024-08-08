d2d1cfac8ed848d6df4e431ad9cae3fa86cbe477
Your Name <youremail@example.com>
2024-08-07 22:01:53 -0400

    server added

---
+++ client/src/Book.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Book() {
+    return (
+      <div className="book">
+        I am a book
+        
+      </div>
+    );
+  }
+
+
+export default Book;
+
+
+
+
